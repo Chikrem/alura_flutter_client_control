90732b09ea9440e219d32da9ad7d12abd844106f
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-01-23 19:42:26 -0300

    Ajustando tipos de clientes

--- lib/models/types.dart
+++ lib/models/types.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 class Types extends ChangeNotifier {
   List<ClientType> types;
 
-  Types({required this.types});
+  Types({
+    required this.types
+  });
 
   void add(ClientType type) {
     types.add(type);
     notifyListeners();
-  } 
-
+  }
 }
