d9687977a40fee757ac84016c1ad433a941350fd
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-01-28 16:32:23 -0300

    Teste de drawer tela inicial -> menu -> tipos de clientes

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -3,6 +3,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:control/main.dart' as app;
 
+// Tela inicial (tela de clientes)
+// Verificar se o drawer está completo
+// Testar a navegação
+// Verificar a tela "Tipos de clientes"
+// Criar um novo tipo de cliente
+// Criar um novo cliente com o novo tipo
+// Verificar o que não é necessário testar
+
 void main (){
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
@@ -10,14 +18,30 @@ void main (){
     app.main();
     await tester.pumpAndSettle();
 
-    expect(find.text('Menu'), findsNothing);
-    await Future.delayed(const Duration(seconds: 2));
+    // Teste Drawer tela inicial
+    expect(find.text('Clientes'), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+
+    // Teste Drawer menu hamburguer
     await tester.tap(find.byIcon(Icons.menu));
-    await Future.delayed(const Duration(seconds: 2));
     await tester.pumpAndSettle();
     expect(find.text('Menu'), findsOneWidget);
-    await Future.delayed(const Duration(seconds: 2));
+    expect(find.text('Gerenciar clientes'), findsOneWidget);
+    expect(find.text('Tipos de clientes'), findsOneWidget);
+    expect(find.text('Sair'), findsOneWidget);
 
+
+    // Teste navegação para tipos de clientes
+    await tester.tap(find.text('Tipos de clientes'));
+    await tester.pumpAndSettle();
+    expect(find.text('Tipos de cliente'), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.text('Platinum'), findsOneWidget);
+    expect(find.text('Golden'), findsOneWidget);
+    expect(find.text('Titanium'), findsOneWidget);
+    expect(find.text('Diamond'), findsOneWidget);
 
   });
 
